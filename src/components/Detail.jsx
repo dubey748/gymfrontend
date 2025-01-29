d0b783a53bad87c5aac87f94bd3f1174ca909677
ABhishek Dubey <dubeyabhishek748@gmail.com>
2025-01-29 16:30:45 +0530

    Rebuild app

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { Typography, Stack, Button } from "@mui/material";
-import BodyPartImage from "../assets/icons/body-part.png";
-import TargetImage from "../assets/icons/target.png";
-import EquipmentImage from "../assets/icons/equipment.png";
+import React from 'react';
+import { Typography, Stack, Button } from '@mui/material';
+
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+
   const extraDetail = [
     {
       icon: BodyPartImage,
@@ -20,50 +22,26 @@ const Detail = ({ exerciseDetail }) => {
       name: equipment,
     },
   ];
+
   return (
-    <Stack
-      gap="60px"
-      sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
-    >
+    <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
-      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
-        <Typography
-          sx={{ fontSize: { lg: "64px", xs: "30px" } }}
-          fontWeight={700}
-          textTransform="capitalize"
-        >
+      <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+        <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
           {name}
         </Typography>
-        <Typography
-          sx={{ fontSize: { lg: "24px", xs: "18px" } }}
-          color="#4F4C4C"
-        >
-          Exercise can help you maintain a healthy weight by burning calories
-          and increasing metabolism. <br />{" "}
-          <span style={{ textTransform: "capitalize" }}>{name}</span> is known
-          to effectively target your {target}. And helps to improve flexibility
-          and balance, which can reduce the risk of falls and injuries.
+        <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+          Exercises keep you strong.{' '}
+          <span style={{ textTransform: 'capitalize' }}>{name}</span> bup is one
+          of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+          <br /> mood and gain energy.
         </Typography>
         {extraDetail?.map((item) => (
           <Stack key={item.name} direction="row" gap="24px" alignItems="center">
-            <Button
-              sx={{
-                background: "#FFF2DB",
-                borderRadius: "50%",
-                width: "100px",
-                height: "100px",
-              }}
-            >
-              <img
-                src={item.icon}
-                alt={bodyPart}
-                style={{ width: "50px", height: "50px" }}
-              />
+            <Button sx={{ background: '#FFF2DB', borderRadius: '50%', width: '100px', height: '100px' }}>
+              <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }} />
             </Button>
-            <Typography
-              textTransform="capitalize"
-              sx={{ fontSize: { lg: "30px", xs: "20px" } }}
-            >
+            <Typography textTransform="capitalize" sx={{ fontSize: { lg: '30px', xs: '20px' } }}>
               {item.name}
             </Typography>
           </Stack>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,12 @@
+const getCachedData = (url) => {
+  const cachedData = localStorage.getItem(url);
+  return cachedData ? JSON.parse(cachedData) : null;
+};
+
+const setCachedData = (url, data) => {
+  localStorage.setItem(url, JSON.stringify(data));
+};
+
 export const exerciseOptions = {
   method: 'GET',
   headers: {
@@ -9,14 +18,19 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '55bb2778e0msh6e073a250858171p13c414jsn1958ae01f500',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
 
 export const fetchData = async (url, options) => {
+  const cachedData = getCachedData(url);
+  if (cachedData) {
+    return cachedData;
+  }
+
   const res = await fetch(url, options);
   const data = await res.json();
-
+  setCachedData(url, data);
   return data;
 };
